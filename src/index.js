69f2543471b1942f69232fd93b1cd8d5c33ba311
Yuliya Shatokhina <59236891+YuShato@users.noreply.github.com>
2021-12-15 01:06:39 +0300

    feat: add Auth0 API

--- src/index.js
+++ src/index.js
@@ -6,11 +6,18 @@ import * as serviceWorker from './serviceWorker'
 import { GithubProvider } from './context/context'
 import { Auth0Provider } from '@auth0/auth0-react'
 
+
 ReactDOM.render(
   <React.StrictMode>
-    <GithubProvider>
-      <App />
-    </GithubProvider>
+    <Auth0Provider
+      domain='dev-uu1jmqjw.us.auth0.com'
+      clientId='4puJb5R4u2vMudxCc7o81cFlaigrEVo2'
+      redirectUri={window.location.origin}
+    >
+      <GithubProvider>
+        <App />
+      </GithubProvider>
+    </Auth0Provider>
   </React.StrictMode>,
   document.getElementById('root')
 )
